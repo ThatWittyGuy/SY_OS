ef7e319316bd8b050593e178839426479186bbb7
ThatWittyGuy <atharva.salitri23@vit.edu>
2024-11-14 17:51:24 +0530

    files

---
+++ LAB PS 1 to 40/14_deadlock_detection.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int main() {
+    int process, resource, i, j, count1 = 0, canProceed;
+    
+    printf("Enter number of processes: ");
+    scanf("%d", &process);
+    printf("Enter number of Resources: ");
+    scanf("%d", &resource);
+
+    int avail[resource], max[process][resource], allot[process][resource], need[process][resource], completed[process];
+
+    for (i = 0; i < process; i++) completed[i] = 0;
+
+    printf("Enter available resource instances: ");
+    for (i = 0; i < resource; i++) scanf("%d", &avail[i]);
+
+    printf("\nEnter request matrix:\n");
+    for (i = 0; i < process; i++) {
+        printf("For P[%d]: ", i);
+        for (j = 0; j < resource; j++) scanf("%d", &max[i][j]);
+    }
+
+    printf("\nEnter allocated matrix:\n");
+    for (i = 0; i < process; i++) {
+        printf("For P[%d]: ", i);
+        for (j = 0; j < resource; j++) {
+            scanf("%d", &allot[i][j]);
+            need[i][j] = max[i][j] - allot[i][j]; 
+        }
+    }
+
+    printf("\nSafe Sequence is: ");
+    while (count1 < process) {
+        int count2 = count1;
+        for (i = 0; i < process; i++) {
+            if (!completed[i]) { 
+                canProceed = 1;
+                for (j = 0; j < resource; j++) {
+                    if (need[i][j] > avail[j]) {
+                        canProceed = 0;
+                        break;
+                    }
+                }
+                if (canProceed) { 
+                    printf("P[%d] ", i);
+                    completed[i] = 1; 
+                    for (j = 0; j < resource; j++) avail[j] += allot[i][j]; 
+                    count1++;
+                }
+            }
+        }
+        if (count1 == count2) {
+            printf("\nDeadlock Detected\n");
+            return 0;
+        }
+    }
+
+    printf("\nNo Deadlock Detected\n");
+    printf("\nSystem is in a safe state.\n");
+    return 0;
+}
---
+++ LAB PS 1 to 40/40_SJF_SRTF.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <climits>
+#include <limits>
+
+using namespace std;
+
+struct Process {
+    int id, arrivalTime, burstTime, remainingTime, completionTime, turnaroundTime, waitingTime;
+    bool isCompleted = false;
+};
+
+bool compareArrival(const Process &a, const Process &b) {
+    return a.arrivalTime < b.arrivalTime;
+}
+
+void displayGanttChart(const vector<pair<int, int>> &ganttChart) {
+    cout << "\nGantt Chart:\n ";
+    for (const auto &entry : ganttChart) cout << "+-------";
+    cout << "+\n|";
+    for (const auto &entry : ganttChart) cout << " P" << entry.first << setw(5) << "|";
+    cout << "\n ";
+    for (const auto &entry : ganttChart) cout << "+-------";
+    cout << "+\n0";
+    for (const auto &entry : ganttChart) cout << setw(8) << entry.second;
+    cout << "\n";
+}
+
+void displayProcessTable(const vector<Process> &processes) {
+    cout << "\nProcess\tArrival\tBurst\tCompletion\tTurnaround\tWaiting\n";
+    for (const auto &p : processes) {
+        cout << "P" << p.id << "\t" << p.arrivalTime << "\t" << p.burstTime << "\t"
+             << p.completionTime << "\t\t" << p.turnaroundTime << "\t\t" << p.waitingTime << "\n";
+    }
+}
+
+void SJF() {
+    int n;
+    cout << "Enter the number of processes for SJF: ";
+    cin >> n;
+
+    vector<Process> processes(n);
+    for (int i = 0; i < n; ++i) {
+        processes[i].id = i + 1;
+        cout << "Enter arrival time and burst time for process P" << processes[i].id << ": ";
+        cin >> processes[i].arrivalTime >> processes[i].burstTime;
+    }
+
+    sort(processes.begin(), processes.end(), compareArrival);
+
+    int currentTime = 0, completed = 0;
+    double totalWT = 0, totalTAT = 0;
+    vector<pair<int, int>> ganttChart;
+
+    while (completed < n) {
+        int idx = -1, minBurstTime = INT_MAX;
+        for (int i = 0; i < n; ++i) {
+            if (!processes[i].isCompleted && processes[i].arrivalTime <= currentTime) {
+                if (processes[i].burstTime < minBurstTime || 
+                    (processes[i].burstTime == minBurstTime && processes[i].arrivalTime < processes[idx].arrivalTime)) {
+                    minBurstTime = processes[i].burstTime;
+                    idx = i;
+                }
+            }
+        }
+
+        if (idx == -1) ++currentTime;
+        else {
+            auto &p = processes[idx];
+            p.completionTime = currentTime + p.burstTime;
+            p.turnaroundTime = p.completionTime - p.arrivalTime;
+            p.waitingTime = p.turnaroundTime - p.burstTime;
+
+            totalWT += p.waitingTime;
+            totalTAT += p.turnaroundTime;
+            p.isCompleted = true;
+            currentTime = p.completionTime;
+            ganttChart.push_back({p.id, p.completionTime});
+            ++completed;
+        }
+    }
+
+    displayProcessTable(processes);
+    displayGanttChart(ganttChart);
+
+    cout << fixed << setprecision(2);
+    cout << "\nAverage Waiting Time: " << totalWT / n << endl;
+    cout << "Average Turnaround Time: " << totalTAT / n << endl;
+}
+
+void SRTF() {
+    int n;
+    cout << "Enter the number of processes for SRTF: ";
+    cin >> n;
+
+    vector<Process> processes(n);
+    for (int i = 0; i < n; ++i) {
+        processes[i].id = i + 1;
+        cout << "Enter arrival time and burst time for process P" << i + 1 << ": ";
+        cin >> processes[i].arrivalTime >> processes[i].burstTime;
+        processes[i].remainingTime = processes[i].burstTime;
+    }
+
+    int completed = 0, current_time = 0, totalWT = 0, totalTAT = 0;
+    vector<pair<int, int>> ganttChart;
+
+    while (completed < n) {
+        int idx = -1, min_time = numeric_limits<int>::max();
+        for (int i = 0; i < n; ++i) {
+            if (processes[i].arrivalTime <= current_time && processes[i].remainingTime > 0 && processes[i].remainingTime < min_time) {
+                min_time = processes[i].remainingTime;
+                idx = i;
+            }
+        }
+
+        if (idx != -1) {
+            if (ganttChart.empty() || ganttChart.back().first != processes[idx].id) {
+                ganttChart.push_back({processes[idx].id, current_time});
+            }
+            processes[idx].remainingTime--;
+            current_time++;
+            if (processes[idx].remainingTime == 0) {
+                processes[idx].completionTime = current_time;
+                processes[idx].turnaroundTime = current_time - processes[idx].arrivalTime;
+                processes[idx].waitingTime = processes[idx].turnaroundTime - processes[idx].burstTime;
+                totalWT += processes[idx].waitingTime;
+                totalTAT += processes[idx].turnaroundTime;
+                completed++;
+                ganttChart.back().second = current_time;
+            }
+        } else {
+            current_time++;
+        }
+    }
+
+    displayProcessTable(processes);
+    displayGanttChart(ganttChart);
+
+    double avgWT = totalWT / (double)n, avgTAT = totalTAT / (double)n;
+    cout << "The average Waiting time : " << avgWT << "\n";
+    cout << "The average Turnaround time : " << avgTAT << "\n";
+}
+
+int main() {
+    // First SJF scheduling
+    SJF();
+
+    // Then SRTF scheduling
+    SRTF();
+
+    return 0;
+}
